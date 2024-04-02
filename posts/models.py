ab7e428dc8912324589b24902acda7aff301e15c
dylan <nyskyline7@gmail.com>
2024-04-03 01:03:40 +0900

    fix: set writer field as FK to User (#2)

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from config.settings import AUTH_USER_MODEL
 
 # Create your models here.
 class BaseModel(models.Model) :
     created_at = models.DateTimeField(verbose_name = "작성일시", auto_now_add = True)
     updated_at = models.DateTimeField(verbose_name = "수정일시", auto_now = True)
+    writer = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
@@ -19,10 +20,9 @@ class Post(BaseModel) :
     id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="제목", max_length=20)
     content = models.TextField(verbose_name="내용")
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(choices=CHOICES, max_length=20)
 
 class Comment(BaseModel) :
     id = models.AutoField(primary_key=True)
     content = models.TextField(verbose_name="내용")
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    
