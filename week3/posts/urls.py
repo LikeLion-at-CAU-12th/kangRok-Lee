b630d4ca3f389e59ed8c9a139e604ea68b931d3b
dylan <nyskyline7@gmail.com>
2024-04-02 21:58:06 +0900

    remove: deprecate week3

--- week3/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from posts.views import *
-
-urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', index, name='my-page'),
-    path('introduction', introduction, name="peer-intro")
-]
