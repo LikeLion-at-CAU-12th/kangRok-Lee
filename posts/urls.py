1b20c7809b1ce4418de28790e826e33673b553aa
dylan <nyskyline7@gmail.com>
2024-04-05 10:39:49 +0900

    feat: [스탠다드 #1] add comment_list

--- posts/urls.py
+++ posts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
+    path('', post_list, name = 'post_list'),
+    path('<int:id>/comments', comment_list, name="comment_list"),
     path('page', index, name='my-page'),
     path('introduction', introduction, name="peer-intro"),
-    path('<int:id>', get_post_detail, name = "게시글 조회"),
+    path('<int:id>', post_detail, name = "게시글 조회"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.http import require_http_methods
 from posts.models import *
 
 # Create your views here.
+import json
 
 def hello_world(request):
     if request.method == "GET":
@@ -52,3 +53,124 @@ def get_post_detail(request,id):
         'message' : '게시글 조회 성공',
         'data' : post_detail_json
     })
+
+@require_http_methods(["POST", "GET"])
+def post_list(request):
+    if request.method == "POST":
+        body = json.loads(request.body.decode('utf-8'))
+
+        # new_post는 QuerySet!
+        new_post = Post.objects.create(
+            writer = body['writer'],
+            title = body['title'],
+            content = body['content'],
+            category = body['category']
+        )
+
+        new_post_json = {
+            # id 는 AutoField
+            "id": new_post.id,
+            "writer": new_post.writer,
+            "title" : new_post.title,
+            "content": new_post.content,
+            "category": new_post.category
+        }
+
+        return JsonResponse({
+            'status': 200,
+            'message': '게시글 생성 성공',
+            'data': new_post_json
+        })
+    
+    if request.method == "GET":
+        post_all = Post.objects.all()
+        post_json_all = []
+
+        for post in post_all:
+            post_json = {
+                "id": post.id,
+                "title" : post.title,
+                "writer": post.writer,
+                "category": post.category
+            }
+            post_json_all.append(post_json)
+
+        return JsonResponse({
+            'status': 200,
+            'message': '게시글 목록 조회 성공',
+            'data': post_json_all
+        })
+
+@require_http_methods(["GET", "PATCH", "DELETE"])
+def post_detail(request, id):
+    if request.method == "GET":
+        post = get_object_or_404(Post, pk=id)
+
+        post_json = {
+            "id": post.id,
+            "writer": post.writer,
+            "title" : post.title,
+            "content": post.content,
+            "category": post.category,
+        }
+
+        return JsonResponse({
+            'status': 200,
+            'message': '게시글 조회 성공',
+            'data': post_json
+        })
+    
+    if request.method == "PATCH":
+        body = json.loads(request.body.decode('utf-8'))
+
+        update_post = get_object_or_404(Post, pk=id)
+
+        update_post.title = body['title']
+        update_post.content = body['content']
+        update_post.category = body['category']
+
+        update_post.save()
+
+        update_post_json = {
+            "id": update_post.id,
+            "writer": update_post.writer,
+            "title": update_post.title,
+            "content": update_post.content,
+            "category": update_post.category
+        }
+
+        return JsonResponse({
+            'status': 200,
+            'message': '게시글 수정 성공',
+            'data': update_post_json
+        })
+    
+    if request.method == "DELETE":
+        delete_post = get_object_or_404(Post, pk=id)
+        delete_post.delete()
+
+        return JsonResponse({
+            'status': 200,
+            'message': '게시글 삭제 성공',
+            'data': None
+        })
+
+@require_http_methods(["GET"])
+def comment_list(request, id):
+    if request.method == "GET":
+        comment_all = Comment.objects.filter(post_id=id)
+        comment_json_all = []
+
+        for comment in comment_all:
+            comment_json = {
+                "id": comment.id,
+                "writer": comment.writer,
+                "content": comment.content
+            }
+            comment_json_all.append(comment_json)
+
+        return JsonResponse({
+            'status': 200,
+            'message': 'fetched all comments',
+            'data': comment_json_all
+        })
