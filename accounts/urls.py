79e570c8eab9e101a230adae74b4f700bcba15d7
dylan <nyskyline7@gmail.com>
2024-06-01 20:37:57 +0900

    add OAuth (0529세션)

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     # 회원가입/로그인/로그아웃
     path("join/", RegisterView.as_view()),
     path("login/", AuthView.as_view()),
-    path('logout/', LogoutView.as_view())
+    path('logout/', LogoutView.as_view()),
+    path("google/login/", google_login, name="google_login"),
+    path("google/callback/", google_callback, name="google_callback"),
 ]
--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,11 @@ THIRD_PARTY_APPS = [
     'corsheaders',
     'rest_framework',
     'drf_yasg',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
 ]
 
 
@@ -84,6 +88,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',  
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -201,3 +206,7 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': False,
     'TOKEN_USER_CLASS': 'accounts.User',
 }
+
+ACCOUNT_EMAIL_REQUIRED = True            # email 필드 사용 o
+ACCOUNT_USERNAME_REQUIRED = True         # username 필드 사용 o
+ACCOUNT_AUTHENTICATION_METHOD = "email"
