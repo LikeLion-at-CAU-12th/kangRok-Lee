0fce07fc690aa8ac8f69e986cac1ec870c4a06a6
dylan <nyskyline7@gmail.com>
2024-06-08 18:02:34 +0900

    feat: add default.storage URL to resource on DB save

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from config.settings import AUTH_USER_MODEL
+from django.core.files.storage import default_storage
 
 # Create your models here.
 class BaseModel(models.Model) :
@@ -25,6 +26,29 @@ class Post(BaseModel) :
     category = models.CharField(choices=CHOICES, max_length=20)
     # image = models.ImageField(verbose_name="사진 첨부", upload_to='images/', null=True, blank=True)
     thumbnail = models.ImageField(null=True, blank=True, verbose_name="썸네일")
+
+    def save(self, *args, **kwargs):
+        file = self.thumbnail
+        if file:
+            self.thumbnail = default_storage.url(file.name) #default_storage points to our S3
+        super().save(*args, **kwargs)
+
+    """
+    TODO: fix
+    response에서
+    "https://qwertycvbnm.s3.ap-northeast-2.amazonaws.com/https%3A/qwertycvbnm.s3.ap-northeast-2.amazonaws.com/quokka.webp"
+    와 같이 default.storage 값이 두번 중복되어 옴
+    """    
+
+    """
+    ++DB에 URL 저장하지 않고 썸네일 전체URL 사용하기
+    @property
+    def thumbnail_url(self):
+        if self.thumbnail and hasattr(self.thumbnail, 'url'):
+            return self.thumbnail.url
+        else:
+            return None
+    """
 
 class Comment(BaseModel) :
     id = models.AutoField(primary_key=True)
